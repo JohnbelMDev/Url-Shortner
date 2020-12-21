ac726d7d78e304975252ced2ea1953297d284490
johnbelmdev <johnbel458@gmail.com>
2020-12-21 15:43:35 -0500

    commit

--- Frontend/urlshortner/src/App.js
+++ Frontend/urlshortner/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import React, {useState} from 'react';
+import React, { useRef, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import './App.css';
 import Axios from 'axios'
@@ -7,26 +7,53 @@ import Axios from 'axios'
 function App() {
   const [input, setInput] = useState('')
   const [show, setShow] = useState('')
+  const [copySuccess, setCopySuccess] = useState('');
+  const textAreaRef = useRef(null);
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand('copy');
+  };
+
+
+
 
   function axious(){
-  Axios({
-  method: 'post',
-  url: 'http://localhost:5000/test',
-  data: {
-    url: input,
-  }
-})
-.then(function (response) {
-  setShow(response.data.shortUrl)
-   console.log(response.data.shortUrl);
-});
+    Axios({
+      method: 'post',
+      url: 'http://localhost:5000/test',
+      data: {
+        url: input,
+      }
+    })
+    .then(function (response) {
+      setShow(response.data.shortUrl)
+      console.log(response.data.shortUrl);
+    });
 
-}
+  }
   return (
     <div className="App">
-    <input onChange = {(e)=>setInput(e.target.value)} />
+    <h2> Url Shortner </h2>
+    <input id="submitId" placeholder="Enter the link shorten"onChange = {(e)=>setInput(e.target.value)} />
     <button onClick = {axious}>Submit</button>
-    <a href={show}>{show} </a>
+    <section>
+    <div>
+
+    <form>
+    <input id="copyId"
+    ref={textAreaRef}
+    value={show}
+    />
+    {
+      document.queryCommandSupported('copy') &&
+      <div>
+      <button id="buttonCopy"onClick={copyToClipboard}>Copy</button>
+      {copySuccess}
+      </div>
+    }
+    </form>
+    </div>
+    </section>
 
     </div>
   );
